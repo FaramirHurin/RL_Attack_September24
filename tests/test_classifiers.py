a1b765115679e838fb46afcdfcf1499ef1b61e5f
Yannick Molinghen <yannick.molinghen@ulb.be>
2026-01-17 13:37:19 +0100

    Notebooks for paper

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -81,39 +81,3 @@ def test_statistical_bounds_accepted():
     assert not np.any(labels)
     s = np.sum(labels)
     assert s == 0, f"Expected no outliers, but found {s} outliers."
-
-
-@pytest.mark.skip
-def test_Isolation_Forest():
-    _params = ClassificationParameters(
-        use_anomaly=True,
-        n_trees=139,
-        balance_factor=0.06473635736763925,
-        contamination="auto",
-        _training_duration=timedelta(days=150),
-        quantiles={
-            "amount": (0.0, 0.9976319783361984),
-            "terminal_risk_last_1 day, 0:00:00": (0.0, 0.9999572867664103),
-        },
-        _rules={
-            timedelta(hours=1): 8,
-            timedelta(days=1): 19,
-            timedelta(weeks=1): 32,
-        },
-    )
-    sys = Banksys.load("../src/cache/banksys/10000-payers/365-days/start-2023-01-01")
-    X = sys.clf.dataset["Transactions"]
-    y = sys.clf.dataset["Labels"]
-
-    X_train, X_test = X[:8000], X[8000:]
-    _y_train, y_test = y[:8000], y[8000:]
-
-    # Fit the Isolation Forest model
-    clf = IsolationForest()
-    clf.fit(X_train)
-
-    y_predict = clf.predict(X_test)
-
-    assert y_predict.shape == y_test.shape
-    assert np.mean(y_predict == -1) < 0.1, "Not too many outliers predicted"
-    assert np.mean(y_predict == 1) > 0.005, "Not too many normal transactions predicted"
