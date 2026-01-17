0dd600a5df4a3a2acd5731a320bce3428f68a8cd
Yannick Molinghen <yannick.molinghen@ulb.be>
2026-01-17 08:42:41 +0100

    VAE parameters details

--- src/parameters/vae_parameters.py
+++ src/parameters/vae_parameters.py
@@ -48,22 +48,27 @@ class VAEParameters:
     def best_vae(anomaly: bool, modification: bool):
         match (anomaly, modification):
             case (False, False):
-                # Optuna trial 82
+                # Optuna trial 82, value=869690.81 (file tuning/vae_tuning.journal)
                 # [latent_dim: 92, hidden_dim: 49, lr: 9.959838177633226e-05, batch_size: 10, num_epochs: 1800, quantile: 0.9131073566439921, generated_size: 44, beta: 0.8432341654489954, n_infiltrated_terminals: 45]
+                # Optuna trial 80, value=873901.74  (file tuning/agents_tuning.journal)
+                # [latent_dim: 90, hidden_dim: 76, lr: 1.4983950667384355e-05, batch_size: 8, num_epochs: 6288, quantile: 0.9933303450751483, generated_size: 525, beta: 0.5686349293691934, n_infiltrated_terminals: 96]
                 return VAEParameters(
-                    latent_dim=92,
-                    hidden_dim=49,
-                    lr=9.959838177633226e-05,
-                    batch_size=10,
-                    num_epochs=1800,
-                    quantile=0.9131073566439921,
+                    latent_dim=90,
+                    hidden_dim=76,
+                    lr=1.4983950667384355e-05,
+                    batch_size=8,
+                    num_epochs=6288,
+                    quantile=0.9933303450751483,
                     supervised=False,
-                    generated_size=44,
-                    n_infiltrated_terminals=45,
-                    beta=0.8432341654489954,
+                    generated_size=525,
+                    n_infiltrated_terminals=96,
+                    beta=0.5686349293691934,
                 )
             case (False, True):
-                # Optuna trial 34
+                # Trial 17, value=685366.01 (file tuning/agents_tuning.journal)
+                # Params = [latent_dim: 36, hidden_dim: 143, lr: 0.0002833361052823374, batch_size: 39, num_epochs: 7779, quantile: 0.9981315336722992, generated_size: 993, beta: 0.5362266193781453, n_infiltrated_terminals: 46]
+
+                # Optuna trial 34, value=843656.42 (file tuning/vae_tuning.journal)
                 # [latent_dim: 11, hidden_dim: 121, lr: 1.1712864680260006e-05, batch_size: 9, num_epochs: 6000, quantile: 0.9953732623329556, generated_size: 91, beta: 0.6087043524048532, n_infiltrated_terminals: 68]
                 return VAEParameters(
                     latent_dim=11,
@@ -78,7 +83,9 @@ class VAEParameters:
                     beta=0.6087043524048532,
                 )
             case (True, False):
-                # Optuna trial 23
+                # Trial 73, value=3692.26 (file tuning/agents_tuning.journal)
+                # [latent_dim: 88, hidden_dim: 146, lr: 1.5072646114347918e-05, batch_size: 58, num_epochs: 1670, quantile: 0.997564842075417, generated_size: 449, beta: 0.15944396195830168, n_infiltrated_terminals: 63]
+                # Optuna trial,23,  value=30023.99 (file tuning/vae_tuning.journal)
                 #  [latent_dim: 66, hidden_dim: 17, lr: 3.2264426994127435e-05, batch_size: 18, num_epochs: 6600, quantile: 0.5985052875072323, generated_size: 29, beta: 0.42151271274078445, n_infiltrated_terminals: 100]
                 return VAEParameters(
                     latent_dim=66,
@@ -93,7 +100,9 @@ class VAEParameters:
                     beta=0.42151271274078445,
                 )
             case (True, True):
-                # Optuna trial 98
+                # Trial 86, value=2078.11 in (file tuning/agents_tuning.journal)
+                # [latent_dim: 57, hidden_dim: 160, lr: 1.0399119421042868e-05, batch_size: 13, num_epochs: 2901, quantile: 0.9796006640858727, generated_size: 466, beta: 0.2979145387502194, n_infiltrated_terminals: 1]
+                # Trial 98, value=90087.71 (file tuning/vae_tuning.journal)
                 # [latent_dim: 55, hidden_dim: 103, lr: 1.1699546326454397e-05, batch_size: 48, num_epochs: 4000, quantile: 0.9993816480073827, generated_size: 69, beta: 0.5420575862666193, n_infiltrated_terminals: 21]
                 return VAEParameters(
                     latent_dim=55,
